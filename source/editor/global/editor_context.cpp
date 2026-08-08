6be881cc22eacfedc7f0500f18c176f8c43520e8
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-08-08 12:29:52 +0700

    Add files via upload

---
+++ source/editor/global/editor_context.cpp
@@ -0,0 +1,36 @@
+#include "editor_context.h"
+#include "engine/function/global/engine_context.h"
+#include "engine/function/render/window_system.h"
+
+namespace Lacrima
+{
+    EditorContext g_editor;
+
+    void EditorContext::init()
+    {
+		
+    }
+
+    void EditorContext::destroy()
+    {
+		
+	}
+
+	void EditorContext::toggleFullscreen()
+	{
+		if (g_engine.isEditor())
+		{
+			m_simulation_panel_fullscreen = !m_simulation_panel_fullscreen;
+		}
+		else
+		{
+			g_engine.windowSystem()->toggleFullscreen();
+		}
+	}
+
+	bool EditorContext::isSimulationPanelFullscreen()
+	{
+		return m_simulation_panel_fullscreen || g_engine.isApplication();
+	}
+
+}
---
+++ source/editor/global/editor_context.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <array>
+
+namespace Lacrima
+{
+    class EditorContext
+    {
+        public:
+            void init();
+            void destroy();
+
+            void toggleFullscreen();
+            bool isSimulationPanelFullscreen();
+
+        private:
+            bool m_simulation_panel_fullscreen = false;
+    };
+
+    extern EditorContext g_editor;
+}
