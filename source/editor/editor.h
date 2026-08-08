094bd0205fb0999f053c6db4bb1bef8c51c6149b
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-08-08 12:28:30 +0700

    Create editor.h

---
+++ source/editor/editor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+namespace Lacrima
+{
+    class Editor
+    {
+        public:
+            void init();
+            void destroy();
+            void run();
+
+        private:
+            void constructUI();
+
+            class Engine* m_engine;
+            std::vector<std::shared_ptr<class EditorUI>> m_editor_uis;
+            std::shared_ptr<class EditorUI> m_simulation_ui;
+    };
+}
